66653f034963ea2c9b2eb5bf2dbeb065fa0e5fe0
Frandy <ydnarf13@hotmail.com>
2024-05-13 16:48:05 -0400

    Llamando API

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 const API_URL = "https://whenisthenextmcufilm.com/api";
 # Inicializar una nueva sesión de cURL; ch = cURL handle
 $ch = curl_init(API_URL);
+// Indicar que queremos recibir el resultado de la petición y no mostrarla en pantalla 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+/* Ejecutar la petición 
+    y guardamos el resultado
+*/
+$result = curl_exec($ch);
+$data = json_decode($result, true);
+
+curl_close($ch);
+
+var_dump($data);
 ?>
 
 <main>
